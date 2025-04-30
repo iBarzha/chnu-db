8194936f348eb371451d87dddbb8764a67d642fc
iBarzha <grytcaie@gmail.com>
2025-04-30 15:09:05 +0300

    add serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
-from .models import Item
+from django.contrib.auth.password_validation import validate_password
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
 
-class ItemSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Item
-        fields = '__all__'
+        model = User
+        fields = ['id', 'username', 'email', 'role', 'first_name', 'last_name']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+
+    class Meta:
+        fields = ['username', 'email', 'password', 'role']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data.get('role', User.Role.STUDENT)
+        )
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['role'] = user.role
+        return token
